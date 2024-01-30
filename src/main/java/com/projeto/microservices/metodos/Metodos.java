7c828069833e926282424b54c514d7e214058ffd
Marcus <mvbrazz@hotmail.com>
2024-01-30 12:38:18 -0300

    Quarta rota, comentando código e implementação de métodos viculados a rota criada

--- src/main/java/com/projeto/microservices/metodos/Metodos.java
+++ src/main/java/com/projeto/microservices/metodos/Metodos.java
@@ -2,18 +2,28 @@ package com.projeto.microservices.metodos;
 
 import java.util.Map;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.HashMap;
+import java.time.ZoneOffset;
+import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.projeto.microservices.entidades.Endereco;
 import org.springframework.beans.factory.annotation.Autowired;
 
+//JWT
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+
+// PDF
+import com.itextpdf.layout.Document;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.io.source.ByteArrayOutputStream;
+
+
 
 @Service
 public class Metodos {
@@ -77,5 +87,27 @@ public class Metodos {
     private Instant genExpirated(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
+
+
+    // Gera PDF
+
+    public byte[] generatePDF() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        try (PdfWriter writer = new PdfWriter(outputStream);
+             PdfDocument pdf = new PdfDocument(writer);
+             Document document = new Document(pdf)) {
+
+            // Conteúdo que você deseja no PDF
+            document.add(new Paragraph("Teste Geração PDF"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return outputStream.toByteArray();
+    }
+
+    
     
 }
--- src/main/java/com/projeto/microservices/rotas/Rotas.java
+++ src/main/java/com/projeto/microservices/rotas/Rotas.java
@@ -3,12 +3,24 @@ package com.projeto.microservices.rotas;
 
 
 import org.springframework.http.ResponseEntity;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 import com.projeto.microservices.entidades.Endereco;
 import com.projeto.microservices.metodos.Metodos;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+//PDF
+import java.io.File;
+import java.io.IOException;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpHeaders;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
@@ -52,7 +64,47 @@ public class Rotas {
         return ResponseEntity.ok(confirmado);
     }
 
-    
+    // Atividade 4 - 1 (Gerar PDF)
+
+    @GetMapping("/gerar-pdf")
+    public ResponseEntity<byte[]> generatePDF() {
+        byte[] pdfContents = metodo.generatePDF();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentLength(pdfContents.length);
+        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=documento.pdf");
+
+        return ResponseEntity.ok()
+            .headers(headers)
+            .contentType(MediaType.APPLICATION_PDF)
+            .body(pdfContents);
+    }
+
+    // Atividade 4 - 2 (Ler PDF)
+
+    @GetMapping("/ler-pdf")
+    public ResponseEntity<String> LerPDF(){
+        // Caminho para o arquivo PDF que será lido
+        String caminhoArquivo = "C://Users//mvbra//Downloads//documento (1).pdf";
+        String textoPagina  = "";
+        // Inicializa o leitor de PDF do iText
+        try (PdfReader reader = new PdfReader(new File(caminhoArquivo));
+             PdfDocument pdfDocument = new PdfDocument(reader)) {
+
+            int numPaginas = pdfDocument.getNumberOfPages();
+
+            // Itera através de todas as páginas e extrai o texto
+            for (int i = 1; i <= numPaginas; i++) {
+                textoPagina = PdfTextExtractor.getTextFromPage(pdfDocument.getPage(i));
+                System.out.println("Conteúdo da página " + i + ":\n" + textoPagina);
+            }
+            return ResponseEntity.ok(textoPagina.toString());
+        } catch (IOException e) {
+            return ResponseEntity.ok("Ocorreu um erro ao ler o arquivo PDF");
+        }
+    }   
 
 
    
